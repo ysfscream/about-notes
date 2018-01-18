4f1bb5a0c8a7850357b17528e691c613e3d78420
ysfscream <894402575@qq.com>
2018-01-18 19:08:40 +0800

    feat: add time

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Personal private blog. ⁄(⁄ ⁄•⁄ω⁄•⁄ ⁄)⁄
 
-❤️ Record love [stories](https://github.com/ysfscream/about-blog/blob/master/about-zhuoyr/%E6%9C%89ZHUOOOYR%20%E5%B0%B1%E4%BC%9A%E6%9C%89%E6%95%85%E4%BA%8B.md)
+❤️ Record love [stories](https://ysfscream.github.io/about-blog/about-zhuoyr/)
 
 📙 Record some [knowledges](https://github.com/ysfscream/about-blog/tree/master/about-knowledg)
 
--- about-zhuoyr/assets/js/index.js
+++ about-zhuoyr/assets/js/index.js
@@ -1,23 +1,24 @@
-const love = '2017-3-3'
-function diffTime (startTime) {
-  const startDate = new Date(startTime)
-  let nowDate = new Date()
-  let loveTime = nowDate.getTime() - startDate.getTime()
+var love = '2017-3-3'
+
+function weloveTime (startTime) {
+  var startDate = new Date(startTime)
+  var nowDate = new Date()
+  var loveTime = nowDate.getTime() - startDate.getTime()
   // 计算出相差天数
-  let days = Math.floor(loveTime / (24 * 3600 * 1000))
+  var days = Math.floor(loveTime / (24 * 3600 * 1000))
   // 计算出小时数 
-  let leave1 = (loveTime % (24 * 3600 * 1000)) // 计算天数后剩余的毫秒数
-  let hours = Math.floor((loveTime % (24 * 3600 * 1000)) / (3600 * 1000))
+  var leave1 = (loveTime % (24 * 3600 * 1000)) // 计算天数后剩余的毫秒数
+  var hours = Math.floor((loveTime % (24 * 3600 * 1000)) / (3600 * 1000))
   // 计算相差分钟数
-  let leave2 = leave1 % (3600 * 1000) // 计算小时数后剩余的毫秒数
-  let minutes = Math.floor(leave2 / (60 * 1000))
+  var leave2 = leave1 % (3600 * 1000) // 计算小时数后剩余的毫秒数
+  var minutes = Math.floor(leave2 / (60 * 1000))
 
   // 计算相差秒数
-  let leave3 = leave2 % (60 * 1000) // 计算分钟数后剩余的毫秒数
-  let seconds = Math.round(leave3 / 1000)
+  var leave3 = leave2 % (60 * 1000) // 计算分钟数后剩余的毫秒数
+  var seconds = Math.round(leave3 / 1000)
   //   console.log(`${days}:${hours}:${minutes}:${seconds}`)
   let timeDisplay = document.getElementById('time')
-  timeDisplay.innerHTML = `💑 ${days} : ${hours} : ${minutes} : ${seconds}`
+  timeDisplay.innerHTML = '💑 ' + days + ' : ' + hours + ' : ' + minutes + ' : ' + seconds
 }
 
-setInterval('diffTime(love)', 1000)
+setInterval('weloveTime(love)', 1000)
